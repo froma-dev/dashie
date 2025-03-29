6ae4db7c6ff89e6f084027e34d557b8dc93856d7
from <frankesau@gmail.com>
2025-03-29 23:40:38 +0100

    change styles

--- src/components/Header/Brand.tsx
+++ src/components/Header/Brand.tsx
@@ -2,10 +2,10 @@ import { IconRocket } from "@tabler/icons-react";
 
 const Brand = () => {
   return (
-    <>
+    <div className="brand">
       <IconRocket size={48} />
       <h1>Dashie</h1>
-    </>
+    </div>
   );
 };
 
