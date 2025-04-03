09124e4074222456bd3a9f4a05c61d43fcb1f36f
froma <github@froma.dev>
2025-04-03 17:33:02 +0200

    import carousel

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import DraggableGrid from "./components/Grid/DraggableGrid";
 import { type DraggableCardProps } from "./components/Card/DraggableCard";
 import Header from "./components/Header/Header";
 import HeroCarousel, { type HeroProps } from "./components/Hero/HeroCarousel";
+import Carousel, { CarouselItemProps } from "./components/Carousel/Carousel";
+import CarouselItem from "./components/Carousel/CarouselItem";
 
 const cards = [
   { id: "card-1", title: "Card", description: "Card description" },
@@ -38,11 +40,90 @@ const heros = [
   },
 ] as HeroProps[];
 
+const carouselItems = [
+  {
+    id: "carousel-1",
+    title: "Carousel 1",
+    description: "Carousel description",
+    image: "https://picsum.photos/600/480?random=1",
+  },
+  {
+    id: "carousel-2",
+    title: "Carousel 2",
+    description: "Carousel description",
+    image: "https://picsum.photos/600/480?random=2",
+  },
+  {
+    id: "carousel-3",
+    title: "Carousel 3",
+    description: "Carousel description",
+    image: "https://picsum.photos/600/480?random=3",
+  },
+  {
+    id: "carousel-4",
+    title: "Carousel 4",
+    description: "Carousel description",
+    image: "https://picsum.photos/600/480?random=4",
+  },
+  {
+    id: "carousel-5",
+    title: "Carousel 5",
+    description: "Carousel description",
+    image: "https://picsum.photos/600/480?random=5",
+  },
+  {
+    id: "carousel-6",
+    title: "Carousel 6",
+    description: "Carousel description",
+    image: "https://picsum.photos/600/480?random=6",
+  },
+  {
+    id: "carousel-7",
+    title: "Carousel 7",
+    description: "Carousel description",
+    image: "https://picsum.photos/600/480?random=7",
+  },
+  {
+    id: "carousel-8",
+    title: "Carousel 8",
+    description: "Carousel description",
+    image: "https://picsum.photos/600/480?random=8",
+  },
+  {
+    id: "carousel-9",
+    title: "Carousel 9",
+    description: "Carousel description",
+    image: "https://picsum.photos/600/480?random=9",
+  },
+  {
+    id: "carousel-10",
+    title: "Carousel 10",
+    description: "Carousel description",
+    image: "https://picsum.photos/600/480?random=10",
+  },
+  {
+    id: "carousel-11",
+    title: "Carousel 11",
+    description: "Carousel description",
+    image: "https://picsum.photos/600/480?random=11",
+  },
+  {
+    id: "carousel-12",
+    title: "Carousel 12",
+    description: "Carousel description",
+    image: "https://picsum.photos/600/480?random=12",
+  },
+] as CarouselItemProps[];
+
 function App() {
   return (
     <>
       <Header />
-      <progress value={0.5} />
+      <Carousel data={carouselItems} title="Carousel">
+        {carouselItems.map((carouselItem) => (
+          <CarouselItem key={carouselItem.id} {...carouselItem} />
+        ))}
+      </Carousel>
       <HeroCarousel data={heros} />
       <Card id={"card-9"} title="Card title" description="Card description" />
       <Link href="https://github.com/">Link</Link>
